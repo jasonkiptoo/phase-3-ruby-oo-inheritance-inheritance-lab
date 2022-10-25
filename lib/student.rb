89d5223f1ce7cf4174e1fd86c43807dc020d3bab
jasonkiptoo <kiptoojason@gmail.com>
2022-10-25 19:58:23 +0300

    Finished assignment without the bonus

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
+require_relative './user'
+
+class Student < User
+  attr_accessor :first_name, :last_name
+    def initialize(knowledge = [])
+        @knowledge = knowledge
+    end
+
+    def learn(info)
+        @knowledge << info
+    end
+
+    def knowledge
+        @knowledge
+    end
 
 end
---
+++ lib/user.rb
@@ -0,0 +1,10 @@
+# require_relative "../bin/time_for_school"
+#  class User <
+class User
+  attr_accessor :firstname, :lastname
+
+  # def initialize (first_name, last_name)
+  #   @first_name = first_name
+  #   @last_name = last_name
+  # end
+end
